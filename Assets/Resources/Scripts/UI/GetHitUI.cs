f52690d8f311050d4b5df187986bc1fa4c358bc5
Toshiki Kado <cad.tsk@gmail.com>
2015-11-16 14:42:27 +0900

    生物兵器、レティクル、残弾、残りHP分かるやーつ、スキルゲージ、リロードUI、攻撃された方向が分かるやーつ、完成

---
+++ Assets/Resources/Scripts/UI/GetHitUI.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GetHitUI : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject player;
+
+	[SerializeField]
+	private float fadeOutTime_secs = 0;
+
+	[SerializeField]
+	private float waitTime_secs = 0;
+
+	private float defaultAngle = 0;
+
+	public float DefaultAngle {
+		set {defaultAngle = value;}
+	}
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (FadeOut ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.rotation = Quaternion.Euler (new Vector3 (0, 0, player.transform.eulerAngles.y) + new Vector3 (0, 0, defaultAngle));
+	}
+
+	private IEnumerator FadeOut () {
+		yield return new WaitForSeconds (waitTime_secs);
+
+		float time = 0;
+		Color imageColor = GetComponent<Image> ().color;
+		while (time < fadeOutTime_secs) {
+			GetComponent<Image> ().color = new Color (imageColor.r, imageColor.g, imageColor.b, Mathf.Lerp (138f / 255f, 0, time / fadeOutTime_secs));
+			time += Time.deltaTime;
+			yield return null;
+		}
+		GetComponent<Image> ().color = new Color (imageColor.r, imageColor.g, imageColor.b, 1);
+	}
+}
