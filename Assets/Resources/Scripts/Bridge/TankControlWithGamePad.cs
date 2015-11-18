a1a1e4ea92daa7f8c968d35430ea469c56ab57a5
FarmiL_t <hiro_shore@yahoo.co.jp>
2015-11-18 13:58:38 +0900

    [ADD] ゲームパッドでコントロールするクラスの基盤を追加

---
+++ Assets/Resources/Scripts/Bridge/TankControlWithGamePad.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankControlWithGamePad : BaseTankControl {
+
+
+	/**********************
+	 *  コンストラクタ
+	 **********************/
+	public TankControlWithGamePad(Tank _tank) : base(_tank){
+		Initialize ();
+	}
+	
+	
+	/**********************
+	 *  初期化処理
+	 **********************/
+	public override void Initialize(){
+
+	}
+	
+	
+	
+	/**********************
+	 *  実行処理
+	 **********************/
+	public override void Execute(){
+	}
+
+}
