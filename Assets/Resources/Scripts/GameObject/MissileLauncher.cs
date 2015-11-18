c9edf7eebc50ff274b4709ed99eb491adab15917
山　雄丞 <nutahuate@yamayuususumu-no-MacBook-Pro.local>
2015-11-18 11:39:28 +0900

    weaponクラスのスペルミスを修正(2度目)、及びレーザー弾が時間経過で消失するように修正。ミサイルを追加

---
+++ Assets/Resources/Scripts/GameObject/MissileLauncher.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MissileLauncher : Weapon {
+
+	public GameObject m_RadarPrefab;
+	public GameObject m_MissilePrefab;
+	
+	private GameObject m_Radar;
+	private LinkedList<GameObject> m_TargetList;
+
+	// Use this for initialization
+	void Start () {
+		m_TargetList = new LinkedList<GameObject>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public override void PullTrigger(){
+		LaunchRadar();
+	}
+
+	public override void ReleaseTrigger(){
+		DeleteRadar();
+		LaunchMissile();
+	}
+
+	public void AddTarget(GameObject target){
+		m_TargetList.AddLast(target);
+	}
+
+	private void LaunchRadar()
+	{
+		if(m_TargetList.Count != 0) m_TargetList.Clear();
+		Debug.Log("Create Radar");
+		m_Radar = (GameObject)Instantiate(m_RadarPrefab,transform.position,transform.rotation);
+		m_Radar.transform.parent = transform;
+	}
+
+	private void DeleteRadar()
+	{
+		Destroy(m_Radar);
+	}
+
+	private void LaunchMissile()
+	{
+		foreach(GameObject x in m_TargetList)
+		{
+			GameObject missile = (GameObject)Instantiate(m_MissilePrefab,transform.position,transform.rotation);
+			missile.GetComponent<MissileLauncherBullet>().SetTarget(x);
+		}
+	}
+}
---
+++ Assets/Resources/Scripts/GameObject/MissileLauncherBullet.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MissileLauncherBullet : MonoBehaviour {
+
+	public float m_MoveSpeed;
+	public float m_SwingSpeedRate;
+	private GameObject m_Target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SetTarget(GameObject target)
+	{
+		m_Target = target;
+		StartCoroutine("Track");
+	}
+
+	private IEnumerator Track()
+	{
+		while(true)
+		{
+//			Quaternion targetVec = transform.position - m_Target.transform.position;
+//			Quaternion.Slerp(transform.rotation,targetVec,Time.deltaTime * m_SwingSpeedRate);
+//			transform.Translate(new Vector3(0.0f,0.0f,m_MoveSpeed));
+			transform.rotation = 
+				Quaternion.Slerp(transform.rotation,
+			                     Quaternion.LookRotation(m_Target.transform.position - transform.position),
+			                     Time.deltaTime*m_SwingSpeedRate);
+
+			transform.position += transform.forward * m_MoveSpeed;
+
+			yield return null;
+		}
+	}
+}
