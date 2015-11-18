a9d46b24ab2412d60b76415df9fa73c17382ee5c
unknown <mashi0214@gmail.com>
2015-11-18 10:21:55 +0900

    ロックオン作成中

---
+++ Assets/Resources/Scripts/UI/Hara/ChaseTarget.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+///--------------------------------------------------------------
+/// <summary>
+/// ターゲットを追うマーカのクラス
+/// </summary>
+///--------------------------------------------------------------
+public class ChaseTarget : MonoBehaviour
+{
+    /*** メンバ変数 ***/
+    public Camera m_mainCamera;
+    public GameObject m_target;
+
+    ///--------------------------------------------------------------
+    /// <summary>
+    /// スタート処理
+    /// </summary>
+    ///--------------------------------------------------------------
+	void Start ()
+    {
+	
+	}
+
+    ///--------------------------------------------------------------
+    /// <summary>
+    /// 更新処理
+    /// </summary>
+    ///--------------------------------------------------------------
+	void Update () 
+    {
+        // ターゲット座標をスクリーン座標に変換
+        Vector3 targetViewportPoint = m_mainCamera.WorldToScreenPoint(m_target.transform.position);
+        transform.position = targetViewportPoint;
+	}
+}
